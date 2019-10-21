a05ffeeb85267c8da5f6385cddd2aef31c25a607
hechtag <jm.hecht@gmx.de>
2019-10-21 18:06:25 +0200

    added metronom-module

---
+++ src/app/metronom/metronom.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MetronomComponent } from './metronom/metronom.component';
+
+
+
+@NgModule({
+  declarations: [MetronomComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MetronomModule { }
